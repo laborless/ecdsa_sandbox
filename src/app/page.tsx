c168faa591fadfd98fa4efbcdfaa2e7d8f2523f4
laborless <wyj1023@gmail.com>
2025-08-03 22:15:55 +0200

    Commented example codes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,7 +134,7 @@ export default function Home() {
  ▀▀▀▀  ▀  ▀ ▀▀ █▪▀▀▀▀▀• ·▀▀▀▀  ▀█▄▀▪•▀▀ ▀▀
         `}
         </pre>
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+        {/* <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
@@ -145,7 +145,7 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
-        </ol>
+        </ol> */}
         <div className="flex flex-col gap-2 w-full">
           <label htmlFor="hexInput" className="font-semibold mb-1">
             Elliptic Curve (Weierstrass equation)
@@ -535,7 +535,7 @@ export default function Home() {
           </ol>
         </div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
+          {/* <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
@@ -557,13 +557,13 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             Read our docs
-          </a>
+          </a> */}
         </div>
       </main>
       <footer
         className={`row-start-3 flex gap-[24px] flex-wrap items-center justify-center ${isDarkMode ? "text-gray-300" : "text-gray-700"}`}
       >
-        <a
+        {/* <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
@@ -607,7 +607,7 @@ export default function Home() {
             height={16}
           />
           Go to nextjs.org →
-        </a>
+        </a> */}
       </footer>
     </div>
   );
